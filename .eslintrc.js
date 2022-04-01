5d21443ed153956534c6a537a2e6935362aa8965
Naoyuki Hayasaka <lelivredes5anneaux@gmail.com>
2022-04-02 08:34:07 +0900

    環境構築

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,23 +2,28 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true
   },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'react-app',
-    'prettier'
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier"
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   plugins: [
-    'react',
+    "react",
   ],
   rules: {
+    // "semi": ["error", "always"],
+    // "quotes": ["error", "double"],
+    "react/prop-types": "error",
   },
 };
